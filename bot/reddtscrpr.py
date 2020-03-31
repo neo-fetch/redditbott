2c9e1b73ec015d350fd083360728da1a7c4273cd
Mayank Deshpande <43036566+mayank-pq2q4@users.noreply.github.com>
2020-03-31 20:50:33 +0530

    Update reddtscrpr.py

--- bot/reddtscrpr.py
+++ bot/reddtscrpr.py
@@ -2,19 +2,19 @@ import telegram
 import telebot
 import praw
 import config
-bot_token = ''
-bot_chatID = '@mk_bot'
+bot_token = 'x'
+bot_chatID = 'x'
 bot = telebot.TeleBot(bot_token)
-
-reddit = praw.Reddit(client_id='XXXXXXXXXXXXXX', \
-                     client_secret='XXXXXXXXXXXXXXXXXXXXXXXX', \
-                     user_agent='your_bot_name', \
-                     username='your_reddit_username', \
-                     password='XXXXXXXXXXXXXX')
+bot.config['api_key'] = bot_token
+reddit = praw.Reddit(client_id='x', \
+                     client_secret='x', \
+                     user_agent='x', \
+                     username='x ', \
+                     password='x')
 
 def reddit_scraper(submission):
     news_data = []
-    subreddit = reddit.subreddit('r/coronavirus')
+    subreddit = reddit.subreddit('Coronavirus')
     new_subreddit = subreddit.new(limit=500)
     for submission in subreddit.new(limit=5):
         data = {}
@@ -33,13 +33,13 @@ def get_msg(news_data):
 
     return msg
 
-subreddit = reddit.subreddit('r/coronavirus')
+subreddit = reddit.subreddit('Coronavirus')
 new_subreddit = subreddit.new(limit=500)
 for submission in subreddit.new(limit=1):
     news_data = reddit_scraper(submission)
     if len(news_data) > 0:
         msg = get_msg(news_data)
-        status = bot.send_message(chat_id='@monsterKing_bot', text=msg, parse_mode=telegram.ParseMode.HTML)        
+        status = bot.send_message(chat_id='@monsterKing_bot', text=msg)        
         if status:            
             print(status)
 else:
