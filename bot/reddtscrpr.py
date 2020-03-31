78d4013ec73ccc3e9e16bc68d30e3be8be328cc0
Mayank Deshpande <43036566+mayank-pq2q4@users.noreply.github.com>
2020-04-01 01:08:35 +0530

    final

--- bot/reddtscrpr.py
+++ bot/reddtscrpr.py
@@ -1,22 +1,38 @@
-import telegram
-import telebot
+from telegram.ext import Updater, CommandHandler
+import requests
+import re
 import praw
-import config
-bot_token = 'x'
-bot_chatID = 'x'
-bot = telebot.TeleBot(bot_token)
-bot.config['api_key'] = bot_token
+import telegram
+api = ""
+contents = requests.get('https://random.dog/woof.json').json()
+image_url = contents['url']
+
+
 reddit = praw.Reddit(client_id='x', \
                      client_secret='x', \
                      user_agent='x', \
-                     username='x ', \
+                     username='x', \
                      password='x')
+
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()    
+    url = contents['url']
+    return url
+
+def get_image_url():
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
 
 def reddit_scraper(submission):
     news_data = []
-    subreddit = reddit.subreddit('Coronavirus')
-    new_subreddit = subreddit.new(limit=500)
-    for submission in subreddit.new(limit=5):
+    subreddit = reddit.subreddit('india')
+  #  new_subreddit = subreddit.new(limit=500)
+    for submission in subreddit.new(limit=10):
         data = {}
         data['title'] = submission.title
         data['link'] = submission.url
@@ -33,14 +49,39 @@ def get_msg(news_data):
 
     return msg
 
-subreddit = reddit.subreddit('Coronavirus')
-new_subreddit = subreddit.new(limit=500)
-for submission in subreddit.new(limit=1):
-    news_data = reddit_scraper(submission)
-    if len(news_data) > 0:
-        msg = get_msg(news_data)
-        status = bot.send_message(chat_id='@monsterKing_bot', text=msg)        
-        if status:            
-            print(status)
-else:
-    print('No updates.')
+def cov(bot, update):
+    chat_id = update.message.chat_id
+    subreddit = reddit.subreddit('india')
+ #   new_subreddit = subreddit.new(limit=500)
+    for submission in subreddit.new(limit=1):
+        news_data = reddit_scraper(submission)
+        if len(news_data) > 0:
+            msg = get_msg(news_data)
+            status = bot.send_message(chat_id= chat_id, text=msg, parse_mode=telegram.ParseMode.HTML)        
+            if status:            
+                print(status)
+            else:
+                print('No updates.')
+
+
+
+def bop(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+    updater = Updater(api)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop',bop))
+    dp.add_handler(CommandHandler('cov',cov))
+    updater.start_polling()
+    updater.idle()
+    
+if __name__ == '__main__':
+    main()
+
+
+
+
+
